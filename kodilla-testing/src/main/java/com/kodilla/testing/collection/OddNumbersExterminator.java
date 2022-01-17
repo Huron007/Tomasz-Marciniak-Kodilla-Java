96ddb06df29c962dbc0658f291062660a8b3d62d
Tomasz Marciniak <huron3572@gmail.com>
2022-01-17 16:35:29 +0100

    6.3 Wersja poprawiona

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class OddNumbersExterminator {
 
-    private static List<Integer> evenNumberList = new ArrayList<>();
+    List<Integer> evenNumberList = new ArrayList<>();
 
 
     public static List<Integer> exterminate(List<Integer> numbers){
+        List<Integer> evenNumberList = new ArrayList<>();
         for(Integer e: numbers){
             if(e % 2 == 0){
                 evenNumberList.add(e);
