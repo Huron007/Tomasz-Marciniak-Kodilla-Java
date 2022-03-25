06ff0c442d03deaf23b272fbce253950fee7f03b
Tomasz Marciniak <huron3572@gmail.com>
2022-03-25 20:36:34 +0100

    14.1 Zadanie

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,18 @@
+package com.kodilla.spring.forum;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String name;
+
+    public ForumUser(){
+        name = "John Smith";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
