3f58132f4c13f57f5cfd465d3a8ab4bece4add3e
Tomasz Marciniak <huron3572@gmail.com>
2022-04-06 21:43:07 +0200

    Zadanie 17.2

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/taskslist/dao/TaskListDao.java
@@ -0,0 +1,16 @@
+package com.kodilla.hibernate.taskslist.dao;
+
+
+import com.kodilla.hibernate.taskslist.TaskList;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Repository
+public interface TaskListDao extends CrudRepository<TaskList, Integer> {
+
+    List<TaskList> findByListName(String listName);
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.taskslist.TaskList;
+import com.kodilla.hibernate.taskslist.dao.TaskListDao;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private final static String LIST_NAME = "test name";
+    private final static String DESCRIPTION = "test description";
+
+    @Test
+    public void testFindByListName(){
+        //Given
+        TaskList taskList = new TaskList(LIST_NAME, DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        int id = taskList.getId();
+        Optional<TaskList> readTaskList = taskListDao.findById(id);
+        Assertions.assertTrue(readTaskList.isPresent());
+
+        //Cleanup
+        taskListDao.deleteById(id);
+    }
+}
